e4968b14fb5185d27106a15a22d7ba894c0a6072
msagres <m_costa3@etu-webschoolfactory.fr>
2023-01-26 14:27:43 +0100

    added tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,44 +6,46 @@ import '@/../styles/Home.module.scss'
 import { Navbar, Button, Link, Text } from '@nextui-org/react'
 
 const Home: NextPage = () => {
-  const { setTheme } = useNextTheme()
-  const { isDark, type } = useTheme()
-  return (
-    <>
-      <Navbar isBordered variant="sticky">
-        <Navbar.Brand>
-          <Text b color="inherit" hideIn="xs">
-            ACME
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content enableCursorHighlight hideIn="xs" variant="highlight-rounded">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link isActive href="#">
-            Customers
-          </Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
+    const { setTheme } = useNextTheme()
+    const { isDark, type } = useTheme()
+    return (
+            <>
+                <Navbar isBordered variant='floating'>
+                    <Navbar.Brand>
+                        <Text b color='inherit' hideIn='xs'>
+                            ACME
+                        </Text>
+                    </Navbar.Brand>
+                    <Navbar.Content enableCursorHighlight hideIn='xs' variant='highlight-rounded'>
+                        <Navbar.Link href='#'>Features</Navbar.Link>
+                        <Navbar.Link isActive href='#'>
+                            Customers
+                        </Navbar.Link>
+                        <Navbar.Link href='#'>Pricing</Navbar.Link>
+                        <Navbar.Link href='#'>Company</Navbar.Link>
+                    </Navbar.Content>
+                    <Navbar.Content>
+                        <Navbar.Link color='inherit' href='#'>
+                            Login
+                        </Navbar.Link>
+                        <Navbar.Item>
+                            <Button auto flat as={ Link } href='#'>
+                                Sign Up
+                            </Button>
+                        </Navbar.Item>
+                    </Navbar.Content>
+                </Navbar>
 
-      <div>
-        The current theme is: {type}
-        <Switch checked={isDark} onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')} />
-      </div>
-      <h1>HOME</h1>
-      <p className="test">test</p>
-    </>
-  )
+                <div className='mt-2'>
+                    The current theme is: { type }
+                    <Switch checked={ isDark } onChange={ ( e ) => setTheme( e.target.checked ? 'dark' : 'light' ) } />
+                </div>
+                <h1 className='text-3xl font-bold underline'>
+                    Hello world!
+                </h1>
+                <p className='test'>test</p>
+            </>
+    )
 }
 
 export default Home
