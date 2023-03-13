4ef62da089389dc2be92be64b128e31d94c5718c
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2023-03-13 21:56:03 +0100

    feat: deploy by sdk

--- components/form/nftDrop.tsx
+++ components/form/nftDrop.tsx
@@ -1,38 +1,59 @@
 import React, { useState } from 'react';
-import { Button, Grid, Input, Spacer } from '@nextui-org/react';
+import { Button, Grid, Input, Spacer, Text } from '@nextui-org/react';
 import { useForm } from 'react-hook-form';
+import { useAddress, useCompilerMetadata, useConnect, useSDK, useUser } from '@thirdweb-dev/react';
 
 
 const NftDrop = () => {
-    const {register, handleSubmit, setError, formState: {isSubmitting, errors}} = useForm();
+    const { register, handleSubmit, setError, formState: { isSubmitting, errors } } = useForm();
+    const sdk = useSDK();
+    const [ { data }, connect ] = useConnect();
+    //const { data: compilerData, isLoading, error } = useCompilerMetadata( contractAddress );
+    const address = useAddress();
 
-    async function saveFormData(data: object) {
-        return await fetch("http://localhost:5000/create-drop", {
-            body: JSON.stringify(data),
-            headers: {"Content-Type": "application/json"},
+    const contractAddress = "{{contract_address}}";
+
+    async function saveFormData( data: object ) {
+        return await fetch( "http://localhost:5000/create-drop", {
+            body: JSON.stringify( data ),
+            headers: { "Content-Type": "application/json" },
             method: "POST"
-        })
+        } )
     }
 
-    const onSubmit = async (data: object) => {
-        const response = await saveFormData(data)
-        if (response.status === 400) {
+
+    const onSubmit = async ( formData: any ) => {
+        /*const response = await saveFormData( data )
+
+        if ( response.status === 400 ) {
             // Validation error
-        } else if (response.ok) {
+        } else if ( response.ok ) {
             // successful
         } else {
             // unknown error
+        }*/
+        if ( data.connected ) {
+            sdk?.deployer.deployNFTDrop( {
+                name: 'my title',
+                description: "my best description",
+                image: '',
+                primary_sale_recipient: address!,
+                fee_recipient: process.env.NEXT_PUBLIC_MADD,
+                seller_fee_basis_points: 1
+            } );
+        } else {
+            console.log( 'no' )
         }
     }
 
     return (
             <>
-                <form onSubmit={ handleSubmit(onSubmit) }>
+                <form onSubmit={ handleSubmit( onSubmit ) }>
                     <Grid.Container justify={ "center" } direction={ "column" }>
                         <Input size={ "lg" }
                                clearable bordered labelPlaceholder="Name"
-                               {...register("name", {required: true})}
-                               />
+                               { ...register( "name", { required: true } ) }
+                        />
                         <Spacer y={ 2 }/>
                         {/*<Input
                                 size={ "lg" } clearable bordered labelPlaceholder="Name" name="name" value={ name }
@@ -50,10 +71,11 @@ const NftDrop = () => {
                         <Input
                                 size={ "lg" } clearable bordered labelPlaceholder="Name" name="name" value={ name }
                                 onChange={ ( e ) => setName( DOMPurify.sanitize( e.target.value ) ) }/>
-                        <Spacer y={ 1 }/>*/}
-                        <Button type="submit">{isSubmitting ? 'Loading' : "Submit"}</Button>
+                        <Spacer y={ 1 }/>*/ }
+                        <Button type="submit">{ isSubmitting ? 'Loading' : "Submit" }</Button>
                     </Grid.Container>
                 </form>
+
             </>
     );
 }
