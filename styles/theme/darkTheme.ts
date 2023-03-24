5869425b8f5643fcfd0e161de7baef3835dabde0
msagres <m_costa3@etu-webschoolfactory.fr>
2023-03-24 23:21:43 +0100

    feat: added secondary custom color

--- styles/theme/darkTheme.ts
+++ styles/theme/darkTheme.ts
@@ -5,6 +5,7 @@ export const DarkTheme = createTheme(
         type: 'dark',
         theme: {
             colors: {
+                //PRIMARY
                 primaryLight: '#FEF098',
                 primaryLightHover: '#FEE665',
                 primaryLightActive: '#FEDC3F',
@@ -15,6 +16,18 @@ export const DarkTheme = createTheme(
                 primarySolidHover: '#B68A00',
                 primarySolidContrast: '$white',
                 primaryShadow: '#FECB00',
+
+                //SECONDARY COLOR
+                secondaryLight: '#FFDE99',
+                secondaryLightHover: '#FFC766',
+                secondaryLightActive: '#FFB03F',
+                secondaryLightContrast: '#DB6D00',
+                secondary: '#ff8b00',
+                secondaryBorder: '#ff8b00',
+                secondaryBorderHover: '#DB6D00',
+                secondarySolidHover: '#B75300',
+                secondarySolidContrast: '$white',
+                secondaryShadow: '#ff8b00',
 
                 gradient: 'linear-gradient(to right, #f7971e, #ffd200)',
                 link: '#d64933',
--- styles/theme/lightTheme.ts
+++ styles/theme/lightTheme.ts
@@ -4,6 +4,7 @@ export const LightTheme = createTheme( {
     type: 'light',
     theme: {
         colors: {
+            //PRIMARY COLOR
             primaryLight: '#FEF098',
             primaryLightHover: '#FEE665',
             primaryLightActive: '#FEDC3F',
@@ -14,6 +15,18 @@ export const LightTheme = createTheme( {
             primarySolidHover: '#B68A00',
             primarySolidContrast: '$white',
             primaryShadow: '#FECB00',
+
+            //SECONDARY COLOR
+            secondaryLight: '#FFDE99',
+            secondaryLightHover: '#FFC766',
+            secondaryLightActive: '#FFB03F',
+            secondaryLightContrast: '#DB6D00',
+            secondary: '#ff8b00',
+            secondaryBorder: '#ff8b00',
+            secondaryBorderHover: '#DB6D00',
+            secondarySolidHover: '#B75300',
+            secondarySolidContrast: '$white',
+            secondaryShadow: '#ff8b00',
 
             gradient: 'linear-gradient(to right, #f7971e, #ffd200)',
             link: '#d64933',
