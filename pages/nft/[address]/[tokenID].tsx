41d70251b1991f3a90c3080ade22cbaa1384df33
msagres <m_costa3@etu-webschoolfactory.fr>
2023-03-26 00:29:47 +0100

    fix: detail nft color text

--- pages/nft/[address]/[tokenID].tsx
+++ pages/nft/[address]/[tokenID].tsx
@@ -8,115 +8,148 @@ import { ChainId } from '@thirdweb-dev/sdk'
 import Header from '../../../components/header/Header'
 
 const NftDetails = () => {
-  const router = useRouter()
-  const { tokenID } = router.query
-  const { contract: marketplace } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_ADRESS, 'marketplace')
-  const { data: item, isLoading } = useListing(marketplace, tokenID as string)
-  const isMismatched = useNetworkMismatch()
-  const [, switchNetwork] = useNetwork()
+	const router = useRouter()
+	const { tokenID } = router.query
+	const { contract: marketplace } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_ADRESS, 'marketplace')
+	const { data: item, isLoading } = useListing(marketplace, tokenID as string)
+	const isMismatched = useNetworkMismatch()
+	const [, switchNetwork] = useNetwork()
 
-  async function buyNft (nftId: BigNumberish) {
-    try {
-      // Ensure user is on the correct network
-      if (isMismatched) {
-        (switchNetwork != null) && await switchNetwork(ChainId.Goerli)
-        return
-      }
+	async function buyNft(nftId: BigNumberish) {
+		try {
+			// Ensure user is on the correct network
+			if (isMismatched) {
+				switchNetwork != null && (await switchNetwork(ChainId.Goerli))
+				return
+			}
 
-      // Simple one-liner for buying the NFT
-      await marketplace?.buyoutListing(BigNumber.from(nftId), 1)
-      alert('NFT bought successfully!')
-    } catch (error) {
-      console.error(error)
-      alert(error)
-    }
-  }
+			// Simple one-liner for buying the NFT
+			await marketplace?.buyoutListing(BigNumber.from(nftId), 1)
+			alert('NFT bought successfully!')
+		} catch (error) {
+			console.error(error)
+			alert(error)
+		}
+	}
 
-  return (
-    <>
-      <Header></Header>
-      <Container>
-        { isLoading
-          ? (
-            <Row justify="center">
-              <Loading type="points" size={ 'lg' }/>
-            </Row>
-            )
-          : (
-            <>
-              <Container css={ { maxWidth: '80%' } }>
-                <Row justify={ 'flex-start' }>
-                  <Text weight={ 'bold' } color={ 'white' }
-                        css={ { backgroundColor: 'black', padding: '0 1rem' } }>
-                    { (item?.asset.properties as { date: string })?.date ?? 'Date missing' }
-                  </Text>
-                </Row>
-                <Spacer y={ 1 }/>
-                <Text size="$3xl" weight={ 'bold' } color={ 'black' }>
-                  { item?.asset.name }
-                </Text>
-                <Spacer y={ 2 }/>
-                <Grid.Container justify="center">
-                  <Grid xs={ 6 }>
-                    <Card>
-                      <Card.Header css={ { position: 'absolute', zIndex: 1, top: 5 } }>
-                        <Col>
-                          <Text size={ 24 } color="white" weight="bold">
-                            <RiMapPinLine/>
-                            { (item?.asset.properties as { location: string })?.location ?? 'Location missing' }
-                          </Text>
-                        </Col>
-                      </Card.Header>
-                      <Card.Image
-                        showSkeleton
-                        src={ item?.asset.image ?? '' }
-                        objectFit="cover"
-                        width="100%"
-                        maxDelay={ 10000 }
-                        height={ 400 }
-                        alt="Card image background"
-                      />
-                    </Card>
-                  </Grid>
-                  <Grid xs={ 6 }>
-                    <Card css={ { marginLeft: '1rem' } }>
-                      <Card.Body>
-                        <Container fluid css={ { maxWidth: '95%' } }>
-                          <Text transform={ 'uppercase' } size={ 24 } color="black"
-                                weight="bold">
-                            { item?.asset.name }
-                          </Text>
-                          <Text size={ 18 } color="black" weight="bold">
-                                                            Le { (item?.asset.properties as { date: string })?.date ?? 'Date missing' }
-                          </Text>
-                          <Text size={ 18 } color="black" weight="bold">
-                            { (item?.asset.properties as { hourStart: string })?.hourStart ?? 'Start Hour Missing' } - { (item?.asset.properties as { hourEnd: string })?.hourEnd ?? 'End Hour Missing' }
-                          </Text>
-                          <Text size={ 18 } color="black" weight="bold">
-                            { (item?.asset.properties as { location: string })?.location ?? 'Location missing' }
-                          </Text>
-                        </Container>
-                      </Card.Body>
-                      <Card.Footer css={ { display: 'flex', justifyContent: 'flex-end' } }>
-                        <Text weight={ 'bold' }>{ item?.buyoutCurrencyValuePerToken.displayValue } { item?.buyoutCurrencyValuePerToken.symbol }</Text>
-                        <Spacer x={ 1 }></Spacer>
-                        <Button onPress={ async () => { await buyNft(BigNumber.from(tokenID)) } }
-                                size={ 'lg' }
-                                shadow
-                                color={ 'primary' } auto>
-                                                        Commander
-                        </Button>
-                      </Card.Footer>
-                    </Card>
-                  </Grid>
-                </Grid.Container>
-              </Container>
-            </>
-            ) }
-
-      </Container>
-    </>
-  )
+	return (
+		<>
+			<Header></Header>
+			<Container>
+				{isLoading ? (
+					<Row justify="center">
+						<Loading
+							type="points"
+							size={'lg'}
+						/>
+					</Row>
+				) : (
+					<>
+						<Container css={{ maxWidth: '80%' }}>
+							<Row justify={'flex-start'}>
+								<Text
+									weight={'bold'}
+									color={'white'}
+									css={{ backgroundColor: 'black', padding: '0 1rem' }}>
+									{(item?.asset.properties as { date: string })?.date ?? 'Date missing'}
+								</Text>
+							</Row>
+							<Spacer y={1} />
+							<Text
+								size="$3xl"
+								weight={'bold'}
+								color={'black'}>
+								{item?.asset.name}
+							</Text>
+							<Spacer y={2} />
+							<Grid.Container justify="center">
+								<Grid xs={6}>
+									<Card>
+										<Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+											<Col>
+												<Text
+													size={24}
+													color="white"
+													weight="bold">
+													<RiMapPinLine />
+													{(item?.asset.properties as { location: string })?.location ??
+														'Location missing'}
+												</Text>
+											</Col>
+										</Card.Header>
+										<Card.Image
+											showSkeleton
+											src={item?.asset.image ?? ''}
+											objectFit="cover"
+											width="100%"
+											maxDelay={10000}
+											height={400}
+											alt="Card image background"
+										/>
+									</Card>
+								</Grid>
+								<Grid xs={6}>
+									<Card css={{ marginLeft: '1rem' }}>
+										<Card.Body>
+											<Container
+												fluid
+												css={{ maxWidth: '95%' }}>
+												<Text
+													transform={'uppercase'}
+													size={24}
+													weight="bold">
+													{item?.asset.name}
+												</Text>
+												<Text
+													size={18}
+													weight="bold">
+													Le{' '}
+													{(item?.asset.properties as { date: string })?.date ??
+														'Date missing'}
+												</Text>
+												<Text
+													size={18}
+													weight="bold">
+													{(item?.asset.properties as { hourStart: string })?.hourStart ??
+														'Start Hour Missing'}{' '}
+													-{' '}
+													{(item?.asset.properties as { hourEnd: string })?.hourEnd ??
+														'End Hour Missing'}
+												</Text>
+												<Text
+													size={18}
+													weight="bold">
+													{(item?.asset.properties as { location: string })?.location ??
+														'Location missing'}
+												</Text>
+											</Container>
+										</Card.Body>
+										<Card.Footer css={{ display: 'flex', justifyContent: 'flex-end' }}>
+											<Text weight={'bold'}>
+												{item?.buyoutCurrencyValuePerToken.displayValue}{' '}
+												{item?.buyoutCurrencyValuePerToken.symbol}
+											</Text>
+											<Spacer x={1}></Spacer>
+											<Button
+												onPress={async () => {
+													await buyNft(BigNumber.from(tokenID))
+												}}
+												size={'lg'}
+												shadow
+												color={'primary'}
+												auto>
+												Commander
+											</Button>
+										</Card.Footer>
+									</Card>
+								</Grid>
+							</Grid.Container>
+						</Container>
+					</>
+				)}
+			</Container>
+		</>
+	)
 }
 
 export default NftDetails
