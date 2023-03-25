86a623c49e599433ab46b17ca5e213a5be53c37d
msagres <m_costa3@etu-webschoolfactory.fr>
2023-03-25 17:51:23 +0100

    refactor: create NftDrop.tsx cleaned added success or error pop-up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'standard-with-typescript',
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest-dom/recommended',
-      'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    /* 'react/jsx-indent-props': ['error', 'first'], */
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      { checksVoidReturn: { attributes: false } }
-    ],
-    quotes: ['error', 'single']
-  }
+	extends: [
+		'eslint:recommended',
+		'standard-with-typescript',
+		'next/core-web-vitals',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jest-dom/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: { attributes: false } }],
+		quotes: ['error', 'single'],
+	},
 }
--- components/nftDrop/NftDrop.tsx
+++ components/nftDrop/NftDrop.tsx
@@ -1,202 +1,154 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Grid, Input, Loading, Modal, Spacer } from '@nextui-org/react'
 import { useForm } from 'react-hook-form'
 import { useAddress, useConnect } from '@thirdweb-dev/react'
-import { NATIVE_TOKEN_ADDRESS, ThirdwebSDK } from '@thirdweb-dev/sdk'
-
-interface formDataType {
-  count: number
-  name: string
-  description: string
-  date: string
-  price: string
-  hourStart: string
-  hourEnd: string
-  location: string
-  image: string
-}
-
-export enum InputEvent {
-  NAME = 'name',
-  DESCRIPTION = 'description',
-  DATE = 'date',
-  COUNT = 'count',
-  PRICE = 'price',
-  HOUR_START = 'hourStart',
-  HOUR_END = 'hourEnd',
-  LOCATION = 'location',
-  IMAGE = 'image'
-}
+import { ThirdwebSDK } from '@thirdweb-dev/sdk'
+import { type formDataType } from '../../models/interfaces/createNFTFormData'
+import { InputName } from '../../models/enum/createNFTInputs'
+import { createNFTicket } from '../../services/createNFTicket'
+import swal from 'sweetalert'
 
 const NftDrop = () => {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<formDataType>()
-  const sdkAdmin = ThirdwebSDK.fromPrivateKey((process.env.NEXT_PUBLIC_SDK_PK ?? ''), 'goerli')
-  const [{ data }] = useConnect()
-  const connectedAddress = useAddress()
-  const [visible, setVisible] = React.useState(false)
-  const handler = () => { setVisible(true) }
+	const {
+		register,
+		handleSubmit,
+		formState: { isSubmitting },
+	} = useForm<formDataType>()
+	const sdkAdmin = ThirdwebSDK.fromPrivateKey(process.env.NEXT_PUBLIC_SDK_PK ?? '', 'goerli')
+	const [{ data: userWallet }] = useConnect()
+	const connectedAddress = useAddress()
+	const [visible, setVisible] = useState(false)
+	const handler = () => {
+		setVisible(true)
+	}
 
-  const closeHandler = () => {
-    setVisible(false)
-    console.log('closed')
-  }
-  // const changeIsLoading = () => { useEffect(() => { isLoading? setIsLoading(false) : setIsLoading(true) }, [])}
+	const closeHandler = () => {
+		setVisible(false)
+		console.log('closed')
+	}
 
-  /*    async function saveFormData( data: formDataType ) {
-            return await fetch( "http://localhost:5000/create-drop", {
-                body: JSON.stringify( data ),
-                headers: { "Content-Type": "application/json" },
-                method: "POST"
-            } )
-        } */
+	const onSubmit = async (formData: formDataType) => {
+		handler()
 
-  const onSubmit = async (formData: formDataType) => {
-    handler()
+		if (userWallet.connected) {
+			await createNFTicket(formData, sdkAdmin, connectedAddress)
+				.then(() => {
+					swal(
+						'Bravo !',
+						formData.count > 1
+							? 'Vos tickets aux étés ajoutés a la blockchain et sont disponibles en vente !'
+							: 'Votre ticket a été ajouté a la blockchain et est disponible en vente !',
+						'success'
+					).catch((e) => {
+						console.error(e)
+					})
+				})
+				.catch((e) => {
+					console.error(e)
+				})
+		} else {
+			swal('', 'Veuillez connecter votre wallet grâce au button du menu principal !', 'error').catch((e) => {
+				console.error(e)
+			})
+			console.log('no connected wallet')
+		}
+		closeHandler()
+	}
 
-    if (data.connected) {
-      const collectionContractAddress = await sdkAdmin?.deployer.deployNFTCollection({
-        name: formData.name,
-        primary_sale_recipient: connectedAddress ?? ''
-      })
-
-      console.log('COLLECTION')
-      const contract = await sdkAdmin?.getContract(collectionContractAddress ?? '', 'nft-collection')
-
-      console.log('CONTRACT')
-
-      const metaData = {
-        name: formData.name,
-        description: formData.description,
-        image: formData.image,
-        properties: {
-          hourEnd: formData.hourEnd,
-          location: formData.location,
-          hourStart: formData.hourStart,
-          date: formData.date
-        }
-      }
-      const metaDatas = []
-
-      for (let i = 0; i < formData.count; i++) {
-        metaDatas.push(metaData)
-      }
-
-      const mintTransaction = await contract?.mintBatchTo(process.env.NEXT_PUBLIC_MADD ?? '', metaDatas)
-
-      console.log('mint')
-      for (const nftObject of mintTransaction) {
-        const marketplaceAddress = process.env.NEXT_PUBLIC_MARKETPLACE_ADRESS
-
-        const marketplace = await sdkAdmin?.getContract(marketplaceAddress ?? '', 'marketplace')
-
-        const listing = {
-          // address of the contract the asset you want to list is on
-          assetContractAddress: collectionContractAddress ?? '',
-          // token ID of the asset you want to list
-          tokenId: (nftObject !== null) ? nftObject.id : 0,
-          // when should the listing open up for offers
-          startTimestamp: new Date(),
-          // how long the listing will be open for
-          listingDurationInSeconds: 86400,
-          // how many of the asset you want to list
-          quantity: 1,
-          // address of the currency contract that will be used to pay for the listing
-          currencyContractAddress: NATIVE_TOKEN_ADDRESS,
-          // how much the asset will be sold for
-          buyoutPricePerToken: formData.price
-        }
-
-        await marketplace?.direct.createListing(listing)
-        console.log('market')
-      }
-    } else {
-      console.log('no connected wallet')
-    }
-    closeHandler()
-  }
-
-  return (
-    <>
-      <Grid.Container justify={ 'center' }>
-        <form onSubmit={
-          handleSubmit(onSubmit)
-        }>
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Nom de l'évènement"
-                 { ...register(InputEvent.NAME, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Description de l'évènement"
-                 { ...register(InputEvent.DESCRIPTION, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Date"
-                 { ...register(InputEvent.DATE, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Count"
-                 { ...register(InputEvent.COUNT, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Price"
-                 { ...register(InputEvent.PRICE, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Hour start"
-                 { ...register(InputEvent.HOUR_START, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Hour end"
-                 { ...register(InputEvent.HOUR_END, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Location"
-                 { ...register(InputEvent.LOCATION, { required: true }) }
-          />
-          <Input size={ 'md' }
-                 clearable
-                 bordered
-                 color={ 'primary' }
-                 labelPlaceholder="Image"
-                 { ...register(InputEvent.IMAGE, { required: true }) }
-          />
-          <Spacer y={ 2 }/>
-          <Button type="submit">{ isSubmitting ? 'Loading' : 'Submit' }</Button>
-        </form>
-        <Modal open={visible} fullScreen >
-          <Modal.Body>
-            <Grid.Container justify={'center'} alignItems={'center'} css={{ height: '100%' }} >
-              <Loading size={'xl'} />
-            </Grid.Container>
-          </Modal.Body>
-        </Modal>
-      </Grid.Container>
-    </>
-  )
+	return (
+		<>
+			<Grid.Container justify={'center'}>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Nom de l'évènement"
+						{...register(InputName.NAME, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Description de l'évènement"
+						{...register(InputName.DESCRIPTION, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Date"
+						{...register(InputName.DATE, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Count"
+						{...register(InputName.COUNT, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Price"
+						{...register(InputName.PRICE, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Hour start"
+						{...register(InputName.HOUR_START, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Hour end"
+						{...register(InputName.HOUR_END, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Location"
+						{...register(InputName.LOCATION, { required: true })}
+					/>
+					<Input
+						size={'md'}
+						clearable
+						bordered
+						color={'primary'}
+						labelPlaceholder="Image"
+						{...register(InputName.IMAGE, { required: true })}
+					/>
+					<Spacer y={2} />
+					<Button type="submit">{isSubmitting ? 'Loading' : 'Submit'}</Button>
+				</form>
+				<Modal
+					open={visible}
+					fullScreen>
+					<Modal.Body>
+						<Grid.Container
+							justify={'center'}
+							alignItems={'center'}
+							css={{ height: '100%' }}>
+							<Loading size={'xl'} />
+						</Grid.Container>
+					</Modal.Body>
+				</Modal>
+			</Grid.Container>
+		</>
+	)
 }
 
 export default NftDrop
