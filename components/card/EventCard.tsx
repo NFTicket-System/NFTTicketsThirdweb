3a1c4389cfe7776a366b80635447948acdbd4225
msagres <m_costa3@etu-webschoolfactory.fr>
2023-03-26 20:46:31 +0200

    style: home card v1

--- components/card/EventCard.tsx
+++ components/card/EventCard.tsx
@@ -1,41 +1,59 @@
 import { Card, Col, Text } from '@nextui-org/react'
+import { useState } from 'react'
 import { type LightEvent } from '../../models/LightEvent'
 
 interface EventCardProps {
-  event: LightEvent
+	event: LightEvent
 }
 
 const EventCard: React.FC<EventCardProps> = (props: EventCardProps) => {
-  return (
-        <Card isPressable isHoverable variant="bordered">
-            <Card.Body>
-                <Card.Image
-                    width="100%"
-                    height="100%"
-                    src={props.event.imageUrl}
-                    alt={props.event.name}
-                    objectFit="cover"/>
-            </Card.Body>
-            <Card.Footer
-            isBlurred
-            css={{
-              position: 'absolute',
-              bgBlur: '#0f111466',
-              borderTop: '$borderWeights$light solid $gray800',
-              bottom: 0,
-              zIndex: 1
-            }}>
-                <Col>
-                    <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-                        New
-                    </Text>
-                    <Text h4 color="white">
-                        { props.event.name }
-                    </Text>
-                </Col>
-            </Card.Footer>
-        </Card>
-  )
+	const [isShown, setIsShown] = useState(false)
+	return (
+		<Card
+			onMouseEnter={() => {
+				setIsShown(true)
+			}}
+			onMouseLeave={() => {
+				setIsShown(false)
+			}}
+			isPressable
+			isHoverable
+			variant="bordered">
+			<Card.Image
+				width="100%"
+				height="100%"
+				src={props.event.imageUrl}
+				alt={props.event.name}
+				objectFit="cover"
+			/>
+			{isShown && (
+				<Card.Footer
+					isBlurred
+					css={{
+						position: 'absolute',
+						bgBlur: '#0f111466',
+						borderTop: '$borderWeights$light solid $gray800',
+						bottom: 0,
+						zIndex: 1,
+					}}>
+					<Col>
+						<Text
+							size={12}
+							weight="bold"
+							transform="uppercase"
+							color="#ffffffAA">
+							New
+						</Text>
+						<Text
+							h4
+							color="white">
+							{props.event.name}
+						</Text>
+					</Col>
+				</Card.Footer>
+			)}
+		</Card>
+	)
 }
 
 export default EventCard
--- components/container/EventContainer.tsx
+++ components/container/EventContainer.tsx
@@ -1,23 +1,35 @@
-import { Grid } from '@nextui-org/react'
+import { Col, Grid, Spacer, Text } from '@nextui-org/react'
 import { type LightEvent } from '../../models/LightEvent'
 import EventCard from '../card/EventCard'
 
 interface EventContainerProps {
-  events: LightEvent[]
+	events: LightEvent[]
 }
 
 const EventContainer: React.FC<EventContainerProps> = (props: EventContainerProps) => {
-  return (
-    <Grid.Container gap={ 2 } justify="space-around">
-      { props.events.map((item) => {
-        return (
-          <Grid key={ item.id } md={ 2 }>
-            <EventCard event={ item }/>
-          </Grid>
-        )
-      }) }
-    </Grid.Container>
-  )
+	return (
+		<Grid.Container
+			gap={2}
+			justify="space-around">
+			{props.events.map((item) => {
+				return (
+					<Grid
+						key={item.id}
+						md={2}>
+						<Col>
+							<Text
+								size="$xl"
+								b>
+								{item.name}
+							</Text>
+							<Spacer y={1} />
+							<EventCard event={item} />
+						</Col>
+					</Grid>
+				)
+			})}
+		</Grid.Container>
+	)
 }
 
 export default EventContainer
