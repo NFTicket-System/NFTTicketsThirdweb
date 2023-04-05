c8728057b62e176688137c89a894421c732a295d
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2023-04-05 15:20:25 +0200

    remove card.body

--- components/card/EventCard.tsx
+++ components/card/EventCard.tsx
@@ -6,46 +6,34 @@ interface EventCardProps {
 }
 
 const EventCard: React.FC<EventCardProps> = (props: EventCardProps) => {
-	return (
-		<Card
-			isPressable
-			isHoverable
-			variant="bordered">
-			<Card.Body>
-				<Card.Image
-					width="100%"
-					height="100%"
-					src={props.event.imageUrl}
-					alt={props.event.name}
-					objectFit="cover"
-				/>
-			</Card.Body>
-			<Card.Footer
-				isBlurred
-				css={{
-					position: 'absolute',
-					bgBlur: '#0f111466',
-					borderTop: '$borderWeights$light solid $gray800',
-					bottom: 0,
-					zIndex: 1,
-				}}>
-				<Col>
-					<Text
-						size={12}
-						weight="bold"
-						transform="uppercase"
-						color="#ffffffAA">
-						New
-					</Text>
-					<Text
-						h4
-						color="white">
-						{props.event.name}
-					</Text>
-				</Col>
-			</Card.Footer>
-		</Card>
-	)
+  return (
+        <Card isPressable isHoverable variant="bordered">
+            <Card.Image
+                width="100%"
+                height="100%"
+                src={props.event.imageUrl}
+                alt={props.event.name}
+                objectFit="cover"/>
+            <Card.Footer
+            isBlurred
+            css={{
+              position: 'absolute',
+              bgBlur: '#0f111466',
+              borderTop: '$borderWeights$light solid $gray800',
+              bottom: 0,
+              zIndex: 1
+            }}>
+                <Col>
+                    <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                        New
+                    </Text>
+                    <Text h4 color="white">
+                        { props.event.name }
+                    </Text>
+                </Col>
+            </Card.Footer>
+        </Card>
+  )
 }
 
 export default EventCard
