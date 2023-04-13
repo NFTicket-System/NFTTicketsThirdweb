4088a4fcb5e4571322f386f87b80a3c1d513eefc
AnisSherpas <anis@sherpas.com>
2023-04-13 13:55:41 +0200

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}],
+    presets: [
+        ['next/babel'],
+        '@babel/preset-env',
         '@babel/preset-typescript',
     ],
 };
