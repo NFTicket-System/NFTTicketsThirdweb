1ace0d862bd8866278e25173db13293dccb48aa9
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2023-06-05 11:44:40 +0200

    Ajout navigation vers page event + mais où est Théo ??

--- components/card/EventCard.tsx
+++ components/card/EventCard.tsx
@@ -1,49 +1,44 @@
-import { Card, Col, Text } from '@nextui-org/react'
-import { type LightEvent } from '../../models/LightEvent'
+import { Card, Text, Col } from "@nextui-org/react"
+import { LightEvent } from "../../models/LightEvent"
+import { useRouter } from "next/router"
 
 interface EventCardProps {
 	event: LightEvent
 }
 
 const EventCard: React.FC<EventCardProps> = (props: EventCardProps) => {
-	return (
-		<Card
-			isPressable
-			isHoverable
-			variant="bordered">
-			<Card.Image
-				width="100%"
-				height="100%"
-				src={props.event.urlImage}
-				alt={props.event.libelle}
-				objectFit="cover"
-			/>
-			<Card.Footer
-				isBlurred
-				css={{
-					position: 'absolute',
-					bgBlur: '#0f111466',
-					borderTop: '$borderWeights$light solid $gray800',
-					bottom: 0,
-					zIndex: 1,
-				}}>
-				<Col>
-					<Text
-						size={12}
-						weight="bold"
-						transform="uppercase"
-						color="#ffffffAA">
-						New
-					</Text>
-					<Text
-						h4
-						color="white">
-						{props.event.libelle}
-					</Text>
-				</Col>
-			</Card.Footer>
-		</Card>
-	)
+
+    const router = useRouter()
+    return(
+        <Card isPressable isHoverable variant="bordered" onClick={()=>{router.push(`/event/${props.event.id}`)}}>
+            <Card.Body>
+                <Card.Image 
+                    width="100%"
+                    height="100%"
+                    src={props.event.imageUrl}
+                    alt={props.event.name}
+                    objectFit="cover"/>
+            </Card.Body>
+            <Card.Footer
+            isBlurred
+            css={{
+                position: "absolute",
+                bgBlur: "#0f111466",
+                borderTop: "$borderWeights$light solid $gray800",
+                bottom: 0,
+                zIndex: 1,
+            }}>
+                <Col>
+                    <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                        New
+                    </Text>
+                    <Text h4 color="white">
+                        { props.event.name }
+                    </Text>
+                </Col>
+            </Card.Footer>
+        </Card>
+    )
 }
 
 export default EventCard
