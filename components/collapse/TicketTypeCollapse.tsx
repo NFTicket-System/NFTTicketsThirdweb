73f8a17b408d506ce92977bc797483b4dc098f7b
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2023-06-27 02:57:08 +0200

    feat: redirect to ticket from event page

--- components/collapse/TicketTypeCollapse.tsx
+++ components/collapse/TicketTypeCollapse.tsx
@@ -1,37 +1,53 @@
-import {Ticket} from "../../models/Event";
-import {Button, Card, Col, Collapse, Row, Spacer, Text} from "@nextui-org/react";
+import { type Ticket } from '../../models/Event'
+import { Button, Collapse, Row, Spacer, Text } from '@nextui-org/react'
 import Image from 'next/image'
-import nextIcon from "../../assets/icons/fleche-droite.png"
+import nextIcon from '../../assets/icons/fleche-droite.png'
 
 interface TicketCardProps {
-    ticketType: string,
-    ticketsOfType: Ticket[],
-    lowerPrice: number
+	ticketType: string
+	ticketsOfType: Ticket[]
+	lowerPrice: number
 }
 
 const TicketTypeCollapse: React.FC<TicketCardProps> = (props: TicketCardProps) => {
-    return (
-            <Collapse title={props.ticketType}>
-                <Row justify="space-between">
-                    <Text color="grey" h6>
-                        Ticket(s) à partir de {props.lowerPrice} €
-                    </Text>
-                    <Button>
-                        <Row justify={"space-between"} align={"center"}>
-                            <Text color="black" b>
-                                Consulter les tickets
-                            </Text>
-                            <Spacer/>
-                            <Image
-                                    width={30}
-                                    height={30}
-                                    src={nextIcon}
-                                    objectFit="cover"/>
-                        </Row>
-                    </Button>
-                </Row>
-            </Collapse>
-    )
+	return (
+		<Collapse title={props.ticketType}>
+			<Row justify="space-between">
+				<Text
+					color="grey"
+					h6>
+					Ticket(s) à partir de {props.lowerPrice} €
+				</Text>
+				<Button
+					onPress={() => {
+						console.log('pressed')
+						const ticket = props.ticketsOfType.find((ticket) => !ticket.solded)
+						console.log(props.ticketsOfType)
+
+						if (ticket !== null) {
+							// void router.push(`/event/${ticket.addressContract}/${ticket.id}`)
+						}
+					}}>
+					<Row
+						justify={'space-between'}
+						align={'center'}>
+						<Text
+							color="black"
+							b>
+							Consulter les tickets
+						</Text>
+						<Spacer />
+						<Image
+							width={30}
+							height={30}
+							src={nextIcon}
+							objectFit="cover"
+						/>
+					</Row>
+				</Button>
+			</Row>
+		</Collapse>
+	)
 }
 
 export default TicketTypeCollapse
--- components/container/TicketTypeContainer.tsx
+++ components/container/TicketTypeContainer.tsx
@@ -1,71 +1,73 @@
-import {Collapse, Container, Text, Card} from "@nextui-org/react";
-import TicketTypeCollapse from "../collapse/TicketTypeCollapse";
-import {Ticket} from "../../models/Event";
+import { Card, Collapse, Container, Text } from '@nextui-org/react'
+import TicketTypeCollapse from '../collapse/TicketTypeCollapse'
+import { type Ticket } from '../../models/Event'
 
 interface TicketTypeContainerProps {
-    tickets: Ticket[],
-    ticketTypes: string[]
+	tickets: Ticket[]
+	ticketTypes: string[]
 }
 
 const TicketTypeContainer: React.FC<TicketTypeContainerProps> = (props: TicketTypeContainerProps) => {
-    const maxContainerSize: number = 3
-    var curentIndex: number = 0
+	const maxContainerSize = 3
+	let curentIndex = 0
 
-    const getTicketsOfType = (type: string): Ticket[] => {
-        const result: Ticket[] = []
-        props.tickets.forEach(it => {
-            if (it.type == type)
-                result.push(it)
-        })
-        return result
-    }
+	const getTicketsOfType = (type: string): Ticket[] => {
+		const result: Ticket[] = []
+		props.tickets.forEach((it) => {
+			if (it.type == type) result.push(it)
+		})
+		return result
+	}
 
-    const getLowestPrice = (tickets: Ticket[]): number => {
-        const cheapestTicket = tickets.reduce(
-                (acc, loc) =>
-                        acc.prix < loc.prix ? acc : loc
-        )
-        return cheapestTicket.prix
-    }
+	const getLowestPrice = (tickets: Ticket[]): number => {
+		const cheapestTicket = tickets.reduce((acc, loc) => (acc.prix < loc.prix ? acc : loc))
+		return cheapestTicket.prix
+	}
 
-    return (
-            <>
-                <Container xl>
-                    <Card>
-                        <Card.Header>
-                            <Container xl>
-                                <Text h2 color="white" b>
-                                    Catégorie de ticket
-                                </Text>
-                            </Container>
-                        </Card.Header>
-                        <Card.Divider/>
-                        <Card.Body>
-                            <Container xl>
-                                <Collapse.Group bordered>
-                                    {props.ticketTypes.map(type => {
-                                        if (curentIndex < maxContainerSize) {
-                                            if (type.length > 0) {
-                                                curentIndex++
-                                                const ticketsOfType = getTicketsOfType(type)
-                                                const lowestPriceOfType = getLowestPrice(ticketsOfType)
-                                                return (
-                                                        <TicketTypeCollapse ticketType={type}
-                                                                            ticketsOfType={ticketsOfType}
-                                                                            lowerPrice={lowestPriceOfType}/>
-                                                )
-                                            } else {
-                                                return <></>
-                                            }
-                                        }
-                                    })}
-                                </Collapse.Group>
-                            </Container>
-                        </Card.Body>
-                    </Card>
-                </Container>
-            </>
-    )
+	return (
+		<>
+			<Container xl>
+				<Card>
+					<Card.Header>
+						<Container xl>
+							<Text
+								h2
+								color="white"
+								b>
+								Catégorie de ticket
+							</Text>
+						</Container>
+					</Card.Header>
+					<Card.Divider />
+					<Card.Body>
+						<Container xl>
+							<Collapse.Group bordered>
+								{props.ticketTypes.map((type, index) => {
+									if (curentIndex < maxContainerSize) {
+										if (type.length > 0) {
+											curentIndex++
+											const ticketsOfType = getTicketsOfType(type)
+											const lowestPriceOfType = getLowestPrice(ticketsOfType)
+											return (
+												<TicketTypeCollapse
+													key={index}
+													ticketType={type}
+													ticketsOfType={ticketsOfType}
+													lowerPrice={lowestPriceOfType}
+												/>
+											)
+										} else {
+											return <></>
+										}
+									}
+								})}
+							</Collapse.Group>
+						</Container>
+					</Card.Body>
+				</Card>
+			</Container>
+		</>
+	)
 }
 
 export default TicketTypeContainer
